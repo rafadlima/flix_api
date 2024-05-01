4c89ca99964e71796beff79d04050e1a5a67c9a3
rafadlima <rafael.limaccont@gmail.com>
2024-05-01 10:04:52 -0300

    First commit

---
+++ movies/serializers.py
@@ -0,0 +1,47 @@
+from django.db.models import Avg
+from rest_framework import serializers
+from movies.models import Movie
+from genres.serializers import GenreSerializer
+from actors.serializers import ActorSerializer
+
+
+class MovieSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Movie 
+        fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError('A data de lançamento não pode ser inferior a 1990.')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError('Resumo não deve ser maior que 200 caracteres.')
+        return value
+
+
+class MovieListDetailSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer()
+    actors = ActorSerializer(many=True)
+    rate = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'genre', 'actors', 'release_date', 'rate', 'resume']
+
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return round(rate, 1)
+        
+        return None
+
+
+class MovieStatsSerializer(serializers.Serializer):
+    total_movies = serializers.IntegerField()
+    movies_by_genre = serializers.ListField()
+    total_reviews = serializers.IntegerField()
+    average_stars = serializers.FloatField()
